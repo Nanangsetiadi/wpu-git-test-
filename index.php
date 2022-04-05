4d040167aed4ad5ad60f8fb22eed15f9bd818416
Nanangsetiadi <sananang820@gmail.com>
2022-04-06 06:34:21 +0700

    menambah list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 </head>
 <body>
     <h1>HELLO WORLD</h1>
+    <p>Pada contoh tersebut, kita sudah berhasil mengembalikan file index.html ke dalam keadaan seperti commit sebelumnya.
+Apabila kita ingin mengembalikan seluruh file dalam commit, kita cukup melakukan checkout ke nomer commit saja, tanpa diikuti nama file. Contoh:</p>
+<li>satu</p>
+<li>dua</li>
 </body>
 </html>
